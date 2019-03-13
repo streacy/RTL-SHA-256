9229b7433651eb19e9e5b3ddc1ed842e923d8973
tgreenid <tabithagreenidge@hotmail.com>
2019-03-13 12:39:30 -0400

    removed files

--- Combination/MessageBlock_inst.vhd
+++
@@ -1,8 +0,0 @@
-MessageBlock_inst : MessageBlock PORT MAP (
-		clock	 => clock_sig,
-		init	 => init_sig,
-		dataout	 => dataout_sig,
-		init_busy	 => init_busy_sig,
-		ram_address	 => ram_address_sig,
-		ram_wren	 => ram_wren_sig
-	);
--- Demofile2/simulation/qsim/work/@compression@function_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity CompressionFunction_vlg_sample_tst is
-    port(
-        clock           : in     vl_logic;
-        lastBlock       : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end CompressionFunction_vlg_sample_tst;
